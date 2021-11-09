0493e02ea9584d4b4dccb7b7e74b46686432cb65
Filipe Beck <beck.filipe@hotmail.com>
2021-11-09 10:27:35 -0300

    argument.go refactor fix

--- cpu/argument.go
+++ cpu/argument.go
@@ -3,7 +3,7 @@ package cpu
 import (
 	"fmt"
 
-	"github.com/theleao/goingboy/gameboy"
+	"github.com/theleao/goingboy/core"
 )
 
 var Arguments []Argument
@@ -41,8 +41,8 @@ type Argument struct {
 	OprndLen int
 	IsMemory bool
 	DataType int
-	readFn   func(*Registers, gameboy.AddressSpace, []int) int
-	writeFn  func(*Registers, gameboy.AddressSpace, []int, int)
+	readFn   func(*Registers, core.AddressSpace, []int) int
+	writeFn  func(*Registers, core.AddressSpace, []int, int)
 }
 
 func InitializeArguments() {
@@ -81,164 +81,164 @@ func NewArgument(label string, oprndLen int, isMemory bool, dataType int) Argume
 
 	switch label {
 	case "A":
-		a.readFn = func(r *Registers, a gameboy.AddressSpace, args []int) int {
+		a.readFn = func(r *Registers, a core.AddressSpace, args []int) int {
 			return r.A
 		}
-		a.writeFn = func(r *Registers, a gameboy.AddressSpace, args []int, c int) {
+		a.writeFn = func(r *Registers, a core.AddressSpace, args []int, c int) {
 			r.A = c
 		}
 	case "B":
-		a.readFn = func(r *Registers, a gameboy.AddressSpace, args []int) int {
+		a.readFn = func(r *Registers, a core.AddressSpace, args []int) int {
 			return r.B
 		}
-		a.writeFn = func(r *Registers, a gameboy.AddressSpace, args []int, c int) {
+		a.writeFn = func(r *Registers, a core.AddressSpace, args []int, c int) {
 			r.B = c
 		}
 	case "C":
-		a.readFn = func(r *Registers, a gameboy.AddressSpace, args []int) int {
+		a.readFn = func(r *Registers, a core.AddressSpace, args []int) int {
 			return r.C
 		}
-		a.writeFn = func(r *Registers, a gameboy.AddressSpace, args []int, c int) {
+		a.writeFn = func(r *Registers, a core.AddressSpace, args []int, c int) {
 			r.C = c
 		}
 	case "D":
-		a.readFn = func(r *Registers, a gameboy.AddressSpace, args []int) int {
+		a.readFn = func(r *Registers, a core.AddressSpace, args []int) int {
 			return r.D
 		}
-		a.writeFn = func(r *Registers, a gameboy.AddressSpace, args []int, c int) {
+		a.writeFn = func(r *Registers, a core.AddressSpace, args []int, c int) {
 			r.D = c
 		}
 	case "E":
-		a.readFn = func(r *Registers, a gameboy.AddressSpace, args []int) int {
+		a.readFn = func(r *Registers, a core.AddressSpace, args []int) int {
 			return r.E
 		}
-		a.writeFn = func(r *Registers, a gameboy.AddressSpace, args []int, c int) {
+		a.writeFn = func(r *Registers, a core.AddressSpace, args []int, c int) {
 			r.E = c
 		}
 	case "H":
-		a.readFn = func(r *Registers, a gameboy.AddressSpace, args []int) int {
+		a.readFn = func(r *Registers, a core.AddressSpace, args []int) int {
 			return r.H
 		}
-		a.writeFn = func(r *Registers, a gameboy.AddressSpace, args []int, c int) {
+		a.writeFn = func(r *Registers, a core.AddressSpace, args []int, c int) {
 			*&r.H = c
 		}
 	case "L":
-		a.readFn = func(r *Registers, a gameboy.AddressSpace, args []int) int {
+		a.readFn = func(r *Registers, a core.AddressSpace, args []int) int {
 			return r.L
 		}
-		a.writeFn = func(r *Registers, a gameboy.AddressSpace, args []int, c int) {
+		a.writeFn = func(r *Registers, a core.AddressSpace, args []int, c int) {
 			*&r.L = c
 		}
 	case "AF":
-		a.readFn = func(r *Registers, a gameboy.AddressSpace, args []int) int {
+		a.readFn = func(r *Registers, a core.AddressSpace, args []int) int {
 			return r.getAF()
 		}
-		a.writeFn = func(r *Registers, a gameboy.AddressSpace, args []int, c int) {
+		a.writeFn = func(r *Registers, a core.AddressSpace, args []int, c int) {
 			r.setAF(c)
 		}
 	case "BC":
-		a.readFn = func(r *Registers, a gameboy.AddressSpace, args []int) int {
+		a.readFn = func(r *Registers, a core.AddressSpace, args []int) int {
 			return r.getBC()
 		}
-		a.writeFn = func(r *Registers, a gameboy.AddressSpace, args []int, c int) {
+		a.writeFn = func(r *Registers, a core.AddressSpace, args []int, c int) {
 			r.setBC(c)
 		}
 	case "DE":
-		a.readFn = func(r *Registers, a gameboy.AddressSpace, args []int) int {
+		a.readFn = func(r *Registers, a core.AddressSpace, args []int) int {
 			return r.getDE()
 		}
-		a.writeFn = func(r *Registers, a gameboy.AddressSpace, args []int, c int) {
+		a.writeFn = func(r *Registers, a core.AddressSpace, args []int, c int) {
 			r.setDE(c)
 		}
 	case "HL":
-		a.readFn = func(r *Registers, a gameboy.AddressSpace, args []int) int {
+		a.readFn = func(r *Registers, a core.AddressSpace, args []int) int {
 			return r.getHL()
 		}
-		a.writeFn = func(r *Registers, a gameboy.AddressSpace, args []int, c int) {
+		a.writeFn = func(r *Registers, a core.AddressSpace, args []int, c int) {
 			r.setHL(c)
 		}
 	case "SP":
-		a.readFn = func(r *Registers, a gameboy.AddressSpace, args []int) int {
+		a.readFn = func(r *Registers, a core.AddressSpace, args []int) int {
 			return r.Sp
 		}
-		a.writeFn = func(r *Registers, a gameboy.AddressSpace, args []int, c int) {
+		a.writeFn = func(r *Registers, a core.AddressSpace, args []int, c int) {
 			r.Sp = c
 		}
 	case "PC":
-		a.readFn = func(r *Registers, a gameboy.AddressSpace, args []int) int {
+		a.readFn = func(r *Registers, a core.AddressSpace, args []int) int {
 			return r.Pc
 		}
-		a.writeFn = func(r *Registers, a gameboy.AddressSpace, args []int, c int) {
+		a.writeFn = func(r *Registers, a core.AddressSpace, args []int, c int) {
 			r.Pc = c
 		}
 	case "d8":
-		a.readFn = func(r *Registers, a gameboy.AddressSpace, args []int) int {
+		a.readFn = func(r *Registers, a core.AddressSpace, args []int) int {
 			return args[0]
 		}
-		a.writeFn = func(r *Registers, a gameboy.AddressSpace, args []int, c int) {
+		a.writeFn = func(r *Registers, a core.AddressSpace, args []int, c int) {
 			panic(fmt.Sprintf("Invalid operation: %s", label))
 		}
 	case "d16":
-		a.readFn = func(r *Registers, a gameboy.AddressSpace, args []int) int {
+		a.readFn = func(r *Registers, a core.AddressSpace, args []int) int {
 			return ToWordBytes(args)
 		}
-		a.writeFn = func(r *Registers, a gameboy.AddressSpace, args []int, c int) {
+		a.writeFn = func(r *Registers, a core.AddressSpace, args []int, c int) {
 			panic(fmt.Sprintf("Invalid operation: %s", label))
 		}
 	case "r8":
-		a.readFn = func(r *Registers, a gameboy.AddressSpace, args []int) int {
+		a.readFn = func(r *Registers, a core.AddressSpace, args []int) int {
 			return ToSigned(args[0])
 		}
-		a.writeFn = func(r *Registers, a gameboy.AddressSpace, args []int, c int) {
+		a.writeFn = func(r *Registers, a core.AddressSpace, args []int, c int) {
 			panic(fmt.Sprintf("Invalid operation: %s", label))
 		}
 	case "a16":
-		a.readFn = func(r *Registers, a gameboy.AddressSpace, args []int) int {
+		a.readFn = func(r *Registers, a core.AddressSpace, args []int) int {
 			return ToWordBytes(args)
 		}
-		a.writeFn = func(r *Registers, a gameboy.AddressSpace, args []int, c int) {
+		a.writeFn = func(r *Registers, a core.AddressSpace, args []int, c int) {
 			panic(fmt.Sprintf("Invalid operation: %s", label))
 		}
 	case "(BC)":
-		a.readFn = func(r *Registers, a gameboy.AddressSpace, args []int) int {
+		a.readFn = func(r *Registers, a core.AddressSpace, args []int) int {
 			return a.GetByte(r.getBC())
 		}
-		a.writeFn = func(r *Registers, a gameboy.AddressSpace, args []int, c int) {
+		a.writeFn = func(r *Registers, a core.AddressSpace, args []int, c int) {
 			a.SetByte(r.getBC(), c)
 		}
 	case "(DE)":
-		a.readFn = func(r *Registers, a gameboy.AddressSpace, args []int) int {
+		a.readFn = func(r *Registers, a core.AddressSpace, args []int) int {
 			return a.GetByte(r.getDE())
 		}
-		a.writeFn = func(r *Registers, a gameboy.AddressSpace, args []int, c int) {
+		a.writeFn = func(r *Registers, a core.AddressSpace, args []int, c int) {
 			a.SetByte(r.getDE(), c)
 		}
 	case "(HL)":
-		a.readFn = func(r *Registers, a gameboy.AddressSpace, args []int) int {
+		a.readFn = func(r *Registers, a core.AddressSpace, args []int) int {
 			return a.GetByte(r.getHL())
 		}
-		a.writeFn = func(r *Registers, a gameboy.AddressSpace, args []int, c int) {
+		a.writeFn = func(r *Registers, a core.AddressSpace, args []int, c int) {
 			a.SetByte(r.getHL(), c)
 		}
 	case "(a8)":
-		a.readFn = func(r *Registers, a gameboy.AddressSpace, args []int) int {
+		a.readFn = func(r *Registers, a core.AddressSpace, args []int) int {
 			return a.GetByte(0xff00 | args[0])
 		}
-		a.writeFn = func(r *Registers, a gameboy.AddressSpace, args []int, c int) {
+		a.writeFn = func(r *Registers, a core.AddressSpace, args []int, c int) {
 			a.SetByte(r.getBC(), c)
 		}
 	case "(a16)":
-		a.readFn = func(r *Registers, a gameboy.AddressSpace, args []int) int {
+		a.readFn = func(r *Registers, a core.AddressSpace, args []int) int {
 			return ToWordBytes(args)
 		}
-		a.writeFn = func(r *Registers, a gameboy.AddressSpace, args []int, c int) {
+		a.writeFn = func(r *Registers, a core.AddressSpace, args []int, c int) {
 			a.SetByte(ToWordBytes(args), c)
 		}
 	case "(C)":
-		a.readFn = func(r *Registers, a gameboy.AddressSpace, args []int) int {
+		a.readFn = func(r *Registers, a core.AddressSpace, args []int) int {
 			return a.GetByte(0xff00 | r.C)
 		}
-		a.writeFn = func(r *Registers, a gameboy.AddressSpace, args []int, c int) {
+		a.writeFn = func(r *Registers, a core.AddressSpace, args []int, c int) {
 			a.SetByte(0xff00|r.C, c)
 		}
 	}
@@ -246,11 +246,11 @@ func NewArgument(label string, oprndLen int, isMemory bool, dataType int) Argume
 	return a
 }
 
-func (a *Argument) Read(reg *Registers, addr gameboy.AddressSpace, args []int) int {
+func (a *Argument) Read(reg *Registers, addr core.AddressSpace, args []int) int {
 	return a.readFn(reg, addr, args)
 }
 
-func (a *Argument) Write(reg *Registers, addr gameboy.AddressSpace, args []int, cntxt int) {
+func (a *Argument) Write(reg *Registers, addr core.AddressSpace, args []int, cntxt int) {
 	a.writeFn(reg, addr, args, cntxt)
 }
 
